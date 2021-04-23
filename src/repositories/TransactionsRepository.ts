f3b334d520b17b513d4800493e50eac6fe196cb4
Natan Junges Muller <natan.junges@hotmail.com>
2021-04-23 18:01:17 -0300

    CR

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,27 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    var income = 0;
+    var outcome = 0;
+    
+    this.transactions.forEach(transaction => {
+      if(transaction.type === 'income')
+        income += transaction.value;
+      else if(transaction.type === 'outcome')
+        outcome += transaction.value;
+    });
+
+    return {income, outcome, total: (income - outcome)};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Request): Transaction {
+    const {income, outcome, total} = this.transactionsRepository.getBalance();
+
+    if(type == 'outcome' && value > total)
+      throw Error('Not able to complete the transaction.');
+    
+    const newTransaction = this.transactionsRepository.create({title, value, type});
+    return newTransaction;
   }
 }
 
